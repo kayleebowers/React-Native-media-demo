9fbd7ae569f1adf36a6d146971fa90b13255d48c
kayleebowers <114454094+kayleebowers@users.noreply.github.com>
2023-09-13 16:59:06 +0000

    feat: add function to get image from library

--- media-demo/App.js
+++ media-demo/App.js
@@ -6,6 +6,16 @@ import { useState } from 'react';
 export default function App() {
   const [image, setImage] = useState(null);
 
+  const pickImage = async () => {
+    let permissions = await ImagePicker.requestMediaLibraryPermissionsAsync();
+    if (permissions?.granted) {
+      let result = await ImagePicker.launchImageLibraryAsync();
+    }
+    if (!result.canceled) {
+      setImage(result.assets[0]);
+    } else setImage(null);
+  }
+  
   return (
     <View style={styles.container}>
       <Button
