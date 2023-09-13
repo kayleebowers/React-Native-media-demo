d8b4842500f3ebb16bd4043954f3dc9b00fdc896
kayleebowers <114454094+kayleebowers@users.noreply.github.com>
2023-09-13 19:13:58 +0000

    feat: give users ability to take new photo

--- media-demo/App.js
+++ media-demo/App.js
@@ -5,8 +5,11 @@ import { useState } from 'react';
 export default function App() {
   const [image, setImage] = useState(null);
 
+  // get photo from library
   const pickImage = async () => {
+    // request permission from user
     let permissions = await ImagePicker.requestMediaLibraryPermissionsAsync();
+    // get selected photo and set state
     if (permissions?.granted) {
       let result = await ImagePicker.launchImageLibraryAsync();
       if (!result.canceled) {
@@ -14,7 +17,24 @@ export default function App() {
       } else setImage(null);
     }
   }
-  
+
+  // take new photo
+  const takePhoto = async () => {
+    // get permission to access camera
+    let permissions = await ImagePicker.getCameraPermissionsAsync();
+    if (permissions?.granted) {
+      // launch camera
+      let result = await ImagePicker.launchCameraAsync();
+      // add new photo to state
+      if (!result.canceled) {
+        setImage(result.assets[0]);
+      } else {
+        setImage(null);
+      }
+    }
+
+  }
+
   return (
     <View style={styles.container}>
       <Button
@@ -23,7 +43,7 @@ export default function App() {
       />
       <Button
         title="Take a photo"
-        onPress={() => {}}
+        onPress={takePhoto}
       />
       {image &&
         <Image source={{uri: image.uri}} style={{ width: 200, height: 200 }}/> 
